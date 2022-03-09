a53fd6ef5edddd87f2a627efcb919894dc761e99
Junhao Zhang <junhaodev@gmail.com>
2022-03-09 17:54:30 -0600

    created basic lexer

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -8,16 +8,81 @@ Lexer::Lexer(std::string code) {
 std::vector<Token> Lexer::lex() {
     while (i < length) {
         char c = peek();
+        next();
 
         switch (c) {
             case '+':
+                new_token(consume('=') ? TType::PlusEq : TType::Plus);
                 break;
             case '-':
+                new_token(consume('=') ? TType::MinusEq : TType::Minus);
+                break;
+            case '*':
+                new_token(consume('*') ? TType::TimesEq : TType::Times);
+                break;
+            case '/':
+                new_token(consume('=') ? TType::DivideEq : TType::Divide);
+                break;
+            case '^':
+                new_token(consume('=') ? TType::PowEq : TType::Pow);
+                break;
+
+            case '(':
+                new_token(TType::LParen);
+                break;
+            case ')':
+                new_token(TType::RParen);
+                break;
+            
+            case '!':
+                new_token(consume('=') ? TType::NotEq : TType::Not);
+                break;
+            case '=':
+                new_token(consume('=') ? TType::EqEq : TType::Eq);
+                break;
+            
+            case '<':
+                new_token(consume('=') ? TType::LessEq : TType::Less);
+                break;
+            case '>':
+                new_token(consume('=') ? TType::GreaterEq : TType::Greater);
+                break;
+            
+            case '&':
+                new_token(TType::And);
+                break;
+            case '|':
+                new_token(TType::Or);
+                break;
+            
+            case '?':
+                new_token(TType::Ques);
+                break;
+            case ':':
+                new_token(TType::Col);
+                break;
+            
+            case '$':
+                new_token(TType::Dollar);
+                break;
+            case '.':
+                new_token(TType::Dot);
+                break;
+            case ',':
+                new_token(TType::Comma);
+                break;
+            
+            case ' ':
+            case '\r':
+            case '\n':
+                continue;
+            
+            default:
                 break;
         }
     }
     
-    tokens.push_back(Token(TType::TEOF, LineInfo(line, col)));
+    new_token(TType::TEOF);
     
     return tokens;
 }
@@ -30,27 +95,22 @@ char Lexer::peek(int n) {
     return code[i + n];
 }
 
+bool Lexer::consume(char c) {
+    if (peek() == c) {
+        next();
+        return true;
+    }
 
-// lineinfo
-LineInfo::LineInfo(size_t line, size_t col) {
-    this->line = line;
-    this->col = col;
+    return false;
 }
 
-LineInfo::LineInfo() {
-    line = col = 0;
+char Lexer::next() {
+    if (code[i] == '\n') line++;
+
+    i++;
+    col++;
 }
 
-
-// tokens
-Token::Token(TType type, LineInfo lf) {
-    this->type = type;
-    this->lf = lf;
-    val = Val();
-}
-
-Token::Token(TType type, LineInfo lf, Val val) {
-    this->type = type;
-    this->lf = lf;
-    this->val = val;
+void Lexer::new_token(TType type, Val val) {
+    tokens.push_back(Token(type, LineInfo(line, col), val));
 }
--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -5,15 +5,14 @@
 #include <vector>
 
 #include "../util/val.hpp"
+#include "token.hpp"
 
-
-class Token;
 
 class Lexer {
     std::string code;
 
-    size_t line;
-    size_t col;
+    size_t line = 1;
+    size_t col = 1;
 
     size_t i = 0;
     size_t length;
@@ -21,94 +20,14 @@ class Lexer {
     std::vector<Token> tokens;
 
     char peek(int n = 0);
+    bool consume(char c);
     char next();
+
+    void new_token(TType type, Val val = Val());
+
 public:
     Lexer(std::string code);
     std::vector<Token> lex();
-};
-
-
-struct LineInfo {
-    size_t line;
-    size_t col;
-
-    LineInfo();
-    LineInfo(size_t line, size_t col);
-};
-
-
-enum class TType {
-    // operators
-    Plus,
-    Minus,
-    Times,
-    Divide,
-    Pow,
-
-    // brackets
-    LParen,
-    RParen,
-
-    // comparisions
-    Not,
-    EqEq,
-    NeEq,
-    LessEq,
-    GreaterEq,
-    Less,
-    Greater,
-
-    // Assignment
-    Eq,
-    PlusEq,
-    MinusEq,
-    TimesEq,
-    DivideEq,
-    PowEq,
-
-    // Logic
-    And,
-    Or,
-    Ques,
-    Col,
-
-    // Other
-    Dollar,
-    Dot,
-    Comma,
-
-    // Keywords
-    For,
-    While,
-
-    If,
-    Elif,
-    Else,
-
-    True,
-    False,
-
-    Ret,
-    Def,
-    
-
-    // Literals
-    String,
-    Number,
-    Identifier,
-
-    TEOF
-};
-
-
-class Token {
-public:
-    LineInfo lf;
-    TType type;
-    Val val;
-
-    Token(TType type, LineInfo lf);
-    Token(TType type, LineInfo lf, Val val);
 };
 
 #endif
